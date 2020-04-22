8d2a1cf002e8d9ed12d04b26a3f64de3ab9c07b5
Rahul Arram <rahul.arram@hopscotch.in>
2020-04-22 07:55:29 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCu9uEN25lUrY7G4RSqH3KsMT6-sSueRqM",
+    authDomain: "image-gallery-7f84e.firebaseapp.com",
+    databaseURL: "https://image-gallery-7f84e.firebaseio.com",
+    projectId: "image-gallery-7f84e",
+    storageBucket: "image-gallery-7f84e.appspot.com",
+    messagingSenderId: "900027645472",
+    appId: "1:900027645472:web:6174ec67d10f2202c60645",
+    measurementId: "G-9YBL4TGR1Z"
+  }
 };
